cfd570a5e23ce512501f89f023e392fd82f80607
Ariful Islam <arifulq234@gmail.com>
2026-02-25 00:58:43 +0600

    version changed due to err

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 // const aboutRoutes = require("./routes/about");
 // const contactRoutes = require("./routes/contact");
