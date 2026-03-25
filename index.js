4a87a343aa8e7be818ccd84557616f2ceee1ddef
siddiquesakib <dmsakib1122@gmail.com>
2026-03-26 02:24:12 +0600

    Update mongodb dependency to version 7.1.1 and clean up code

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ const authRoutes = require("./routes/auth");
 const ordersRoutes = require("./routes/orders");
 const promoRoutes = require("./routes/promo");
 const runAuctionCheck = require("./routes/scheduledTask");
-
 // Root endpoint
 app.get("/", (req, res) => {
   res.send("Welcome to the UnityShop API!");
@@ -100,6 +99,7 @@ app.use("/group-buy", require("./routes/groupBuy"));
 app.use("/promo", promoRoutes);
 app.use("/reviews", require("./routes/reviews"));
 app.use("/bids", require("./routes/bids"));
+// app.use('/scheduled-tasks', require('./routes/scheduledTask'));
 
 // Import Socket Handlers
 const productViewerSocket = require("./sockets/productViewer");
@@ -124,15 +124,15 @@ io.on("connection", (socket) => {
   });
 });
 
+// Start Server
 // Start Server and Database Connection immediately
 server.listen(port, async () => {
   console.log(`Server running on port ${port}`);
   try {
+    // সার্ভার চালু হওয়ার সাথে সাথেই ডাটাবেস কানেক্ট করবে এবং ক্রন জব শুরু করবে
     await connectToDatabase();
-    console.log("Background tasks (Auction Check) initialized.");
   } catch (err) {
     console.error("Initial DB connection failed:", err);
   }
 });
-
 module.exports = app;
